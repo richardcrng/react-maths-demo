4831770d99e6fd2facfe377025c1511db925070e
Richard Ng <richardcrng@gmail.com>
2019-07-22 16:26:32 +0100

    install redux packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Demo from './Demo';
+import store from './redux/store';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+function App() {
+  return (
+    <Provider store={store}>
+      <Demo />
+    </Provider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
